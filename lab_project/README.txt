af84fa2e5d796f754e6ca0f7d76a299f15f3f1b5
Rui Pedro <99.ruipedro@gmail.com>
2021-04-02 00:53:59 +0100

    The JSON from the API is shown on the web page

---
+++ lab_project/README.txt
@@ -0,0 +1,9 @@
+TO RUN:
+
+cd lab_project/complete
+./mvnw spring-boot:run
+
+
+ON BROWSER:
+
+http://localhost:8080
---
+++ lab_project/complete/src/main/java/com/example/servingwebcontent/OpenSky.java
@@ -0,0 +1,75 @@
+package com.example.servingwebcontent;
+
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import com.google.gson.Gson; 
+
+public class OpenSky {
+
+    
+    public static String getJson() {
+
+        /*
+        String inline = "";
+
+        try {
+            URL url = new URL("https://opensky-network.org/api");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responsecode = conn.getResponseCode();
+            if(responsecode != 200)
+                throw new RuntimeException("HttpResponseCode: " +responsecode);
+            else {
+                Scanner sc = new Scanner(url.openStream());
+                while(sc.hasNext()) {
+                    inline+=sc.nextLine();
+                }
+                sc.close();
+            }
+        } catch(IOException ex) {
+             
+        }
+        */
+
+        String output = "";
+
+        try {
+            String url = "https://opensky-network.org/api/states/all";
+
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Erro " + conn.getResponseCode() + " ao obter dados da URL " + url);
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            //String output = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line;
+            }
+
+            conn.disconnect();
+            /*
+            Gson gson = new Gson();
+            Dados dados = gson.fromJson(new String(output.getBytes()), Dados.class);
+
+            System.out.println("TIME: " + dados.getTime());
+            System.out.println("STATES: " + Arrays.toString(dados.getStates()[0]));
+            */
+        } catch (IOException ex) {
+            //Logger.getLogger(APIRest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return output;
+    }
+
+}
