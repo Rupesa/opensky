e7b82827d2e87b6590afef3e87451d3f550a9f68
Rui Pedro <99.ruipedro@gmail.com>
2021-04-11 15:48:22 +0100

    /flights added

--- lab_project/complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ lab_project/complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 
-	@MessageMapping("/hello")
+	/* @MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	public Greeting greeting1(HelloMessage message) throws Exception {
 		Thread.sleep(1000); // simulated delay
 		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-	}
+	} */
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -38,6 +38,8 @@ public class GreetingController {
 		return "greeting";
 	}
 
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
 	@GetMapping("/flights")
 	public String flights(@RequestParam(name="name", required=false, defaultValue="from Rupesa")String name, Model model) {
 	
@@ -46,8 +48,8 @@ public class GreetingController {
 
 		model.addAttribute("teste", "glugluglu");
 		model.addAttribute("time", dados.getTimeFormated());
-                model.addAttribute("states", dados.getStates());
-		return "greeting";
+        model.addAttribute("states", dados.getStates());
+		return "flights";
 	}
 
 	/* @MessageMapping("/hello")
